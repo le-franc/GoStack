40eea350b9c7c6459190836136d375c3ee8e95d1
Leandro <leandrofrancisco@iMac-de-Leandro.local>
2020-02-01 19:40:11 -0300

    Tratamento de Avatar

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -3,6 +3,7 @@ import multer from 'multer';
 import multerConfig from './config/multer';
 import UserControler from './app/controllers/userController';
 import SessionControler from './app/controllers/sessionController';
+import FileController from './app/controllers/fileController';
 import authMiddleware from './app/middleware/auth';
 
 const routes = new Router();
@@ -15,8 +16,6 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserControler.update);
 
-routes.post('/files', upload.single('file'),yarn (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
