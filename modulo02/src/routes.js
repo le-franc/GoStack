1d51b952353edc6b05a3930f46a6af31da43bf43
Leandro <leandrofrancisco@iMac-de-Leandro.local>
2020-02-01 12:47:16 -0300

    Add feature - Multer

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 import UserControler from './app/controllers/userController';
 import SessionControler from './app/controllers/sessionController';
 import authMiddleware from './app/middleware/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserControler.store);
 routes.post('/sessions', SessionControler.store);
@@ -11,5 +14,9 @@ routes.post('/sessions', SessionControler.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserControler.update);
+
+routes.post('/files', upload.single('file'),yarn (req, res) => {
+  return res.json({ ok: true });
+});
 
 export default routes;
