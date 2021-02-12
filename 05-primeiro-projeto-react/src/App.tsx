ca1bd93111bf0a8feb0c17ae8a5531130b82afb8
Leandro <leandro@ecosis.com.br>
2021-02-12 16:32:30 -0300

    router-features

--- 05-primeiro-projeto-react/src/App.tsx
+++ 05-primeiro-projeto-react/src/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
+import Routes from './routes'
 
-function App() {
-  return (
-    <h1>Hello World</h1>
-  );
-}
+const App: React.FC = () =>  <Routes />;
 
 export default App;
