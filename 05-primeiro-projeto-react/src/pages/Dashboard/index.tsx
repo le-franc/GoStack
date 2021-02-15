627a3330a8744c032a8190b3bb7d1e4295ead47f
Leandro <leandro@ecosis.com.br>
2021-02-15 10:49:27 -0300

    general-adjusts

--- 05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ 05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,8 +1,54 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-    return <Title>Explore repositórios no Github</Title>;
+    return (
+        <>
+            <img src={logoImg} alt="GitHub Explorer" />
+            <Title>Explore repositórios no Github</Title>
+            <Form action="">
+                <input placeholder="Digite o nome do Repositório"></input>
+                <button type="submit">Pesquisar</button>
+            </Form>
+            <Repositories>
+                <a href="teste">
+                    <img
+                        src="https://avatars.githubusercontent.com/u/59741551?s=460&u=14f07116c155b1bc87e82f1f18359c4004a3753c&v=4"
+                        alt="Leandro Francisco"
+                    />
+                    <div>
+                        <strong>Leandro/GoStack</strong>
+                        <p>Meu repositório</p>
+                    </div>
+                    <FiChevronRight />
+                </a>
+                <a href="teste">
+                    <img
+                        src="https://avatars.githubusercontent.com/u/59741551?s=460&u=14f07116c155b1bc87e82f1f18359c4004a3753c&v=4"
+                        alt="Leandro Francisco"
+                    />
+                    <div>
+                        <strong>Leandro/GoStack</strong>
+                        <p>Meu repositório</p>
+                    </div>
+                    <FiChevronRight />
+                </a>
+                <a href="teste">
+                    <img
+                        src="https://avatars.githubusercontent.com/u/59741551?s=460&u=14f07116c155b1bc87e82f1f18359c4004a3753c&v=4"
+                        alt="Leandro Francisco"
+                    />
+                    <div>
+                        <strong>Leandro/GoStack</strong>
+                        <p>Meu repositório</p>
+                    </div>
+                    <FiChevronRight />
+                </a>
+            </Repositories>
+        </>
+    );
 };
 
 export default Dashboard;
